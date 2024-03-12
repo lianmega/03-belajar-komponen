e4a4bfff2c8d9061ff1f2375ed8f1a5b0b9cbe48
Lian2041720257 <2041720257@student.polinema.ac.id>
2024-03-12 11:54:15 +0700

    W03: Jawaban soal 1

---
+++ 03-belajar-komponen/belajar-komponen/show.image.js
@@ -0,0 +1,5 @@
+const ShowImage = () => {
+    return(
+        <Profile/>
+    );
+}
---
+++ 03-belajar-komponen/belajar-komponen/src/app/page.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import Profile from "../components/profile";
+
+export default function Home() {
+  return (
+    <>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <div style={{textAlign: 'center'}}>
+          <h1> Ilmuan yang luar biasa </h1>
+        </div>
+
+        <div className="flex w-full justify-center">
+          <Profile/>
+          <Profile/>
+          <Profile/>
+        </div>
+
+        <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+        </div>
+      </main>
+    </>
+  );
+}
